e570f06f0a94ddbc5bb15abd4bc4ccb98f61431e
NikithaChigurupati <88763768+NikithaChigurupati@users.noreply.github.com>
2021-08-11 12:04:08 +0530

    cachematrix.r

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Following two functions, makeCacheMatrix and cacheSolve, are to be used to
+# cache the inverse of a matrix.
 
-## Write a short comment describing this function
+# In the following function the arguments of the list does;
+# set, sets the matrix value
+# get, gets the matrix value
+# setinverse, sets the inverse matrix value
+# getinverse, gets the inverse matrix value
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix())
+  {
+  i <- NULL
+  set <- function(y)
+      
+    {
+    x <<- y 
+    i <<- NULL  
+    }  
+    
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  }
 
-}
 
+# The following function returns the inverse of matrix.
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...)
+  {
+  i <- x$getinverse()
+  if (!is.null(i))  
+      
+    {  
+    message("Getting Cached Data") 
+    return(i) 
+    }
+    
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i 
+  }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Example
+# The first expression creates a randomized 2x2 matrix.
+
+x <- matrix(rexp(4, rate=.1), ncol=2)
+y <- makeCacheMatrix(x)
+z <- cacheSolve(y)
+
+# An example for a random matrix;
+#           [,1]       [,2]
+# [1,]  3.329605  0.4603402
+# [2,] 22.225423 14.1100545
+
+# Inversion of said matrix using previously written functions;
+#            [,1]        [,2]
+# [1,]  0.3839507 -0.01252638
+# [2,] -0.6047791  0.09060235
